5687b6df24576e11818c4d548abb0d45b5a589ba
Robert B Garcia <robgarcia552@gmail.com>
2020-05-09 13:54:29 -0400

    initial navbar

--- client/src/components/NavbarComponent.js
+++ client/src/components/NavbarComponent.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import logo from '../assets/geek-wallet-logo.png';
 
 export default function Navbar() {
   return(
-    <navbar className='navbar'>
-      <a href='/'>Home</a>
-      <a href='/about'>About</a>
-    </navbar>
+    <div className='navbar'>
+      <div className='flex-container'>
+        <img src={logo} className='nav-logo' />
+        <h1 className='nav-header'>Geek Wallet</h1>
+      </div>
+      <div className='flex-container'>
+        <a href='/' className='nav-link'>Home</a>
+        <a href='/about' className='nav-link'>About</a>
+      </div>
+    </div>
   )
 }
--- client/src/styles/global.js
+++ client/src/styles/global.js
@@ -23,8 +23,9 @@ export default createGlobalStyle`
     font: 16px 'Montserrat', sans-serif;
     letter-spacing: .5px;
   }
-  a {
+  * a:link, * a:visited, * a:hover, * a:active {
     text-decoration: none;
+    color: black;
   }
   ul {
     list-style: none;
@@ -32,12 +33,37 @@ export default createGlobalStyle`
   button {
     cursor: pointer;
   }
+
+
+  .flex-container {
+    display: flex;
+    justify-content: space-around;
+  }
+  .navbar {
+    text-align: center;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 16px;
+  }
+  .nav-link {
+    padding: 20px;
+  }
+  .nav-logo {
+    height: 40px;
+    width: 80px;
+    display: inline;
+    margin: auto;
+  }
+  .nav-header {
+    font-size: 1.5em;
+    margin: auto;
+    padding: 10px;
+  }
   .primary-btn {
     background: #008255;
     padding: 10px;
-    font-family: 'Montserrat', sans-serif;
-    letter-spacing: .5px;
-    font-size: 16px;
   
     & a:link, & a:visited, & a:hover, & a:active {
       text-decoration: none;
