adee9d6cf46340317ae870bea60401978c2c3e02
Robert B Garcia <robgarcia552@gmail.com>
2020-05-09 10:34:29 -0400

    initial user model

---
+++ backend/models/user.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,    
+  },
+  birthDate: {
+    type: Date,
+    required: true,
+  },
+  articles: {
+    dateWritten: {
+      type: Date,
+    },
+    article: {
+      type: String,
+    },
+  },
+},
+  {
+    timestamps: true,
+  },
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
